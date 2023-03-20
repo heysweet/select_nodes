132bc63e16d9e4875996063dc72e7f86c4e4eca1
Andrew Sweet <andrewmsweet@gmail.com>
2023-03-20 06:49:30 -0400

    Invert parents into children

--- src/graph.rs
+++ src/graph.rs
@@ -6,7 +6,37 @@ use std::collections::{HashMap, HashSet};
 #[derive(Clone, Debug, Eq, PartialEq, Ord, PartialOrd, Hash)]
 pub struct UniqueId(String);
 
+#[derive(Clone, Debug, Eq, PartialEq)]
 pub struct Graph {
+    children_map: HashMap<UniqueId, HashSet<UniqueId>>,
     /// A map of nodes to its set of parents
     parents_map: HashMap<UniqueId, HashSet<UniqueId>>,
+}
+
+impl Graph {
+    fn get_children_from_parents(parents_map: &HashMap<UniqueId, HashSet<UniqueId>>) -> HashMap<UniqueId, HashSet<UniqueId>> {
+        let mut children_map: HashMap<UniqueId, HashSet<UniqueId>> = HashMap::new();
+
+        for (child_id, parent_ids) in parents_map.clone().iter() {
+            for parent_id in parent_ids {
+                let value = children_map.get_mut(&parent_id);
+                match value {
+                    Some(children) => {
+                        children.insert(child_id.clone());
+                    },
+                    None => {
+                        let mut children = HashSet::new();
+                        children.insert(child_id.clone());
+                        children_map.insert(child_id.clone(), children);
+                    }
+                }
+            }
+        }
+        children_map
+    }
+
+    pub fn new(parents_map: HashMap<UniqueId, HashSet<UniqueId>>) -> Self {
+        let children_map = Self::get_children_from_parents(&parents_map);
+        Graph { parents_map, children_map }
+    }
 }
