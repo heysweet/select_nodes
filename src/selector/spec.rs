fe611f90d5280b06248b6f1bd6401d16c01bcb48
Andrew Sweet <andrewmsweet@gmail.com>
2023-03-20 21:01:53 -0400

    Start on builder type for selection criteria

--- src/selector/spec.rs
+++ src/selector/spec.rs
@@ -1,7 +1,9 @@
+use std::collections::VecDeque;
+
 /// core/dbt/graph/selector_spec.py
 
 use super::{MethodName, FQNMethod, FileMethod, PathMethod};
-use regex::Regex;
+use regex::{Regex, Captures};
 
 lazy_static! {
     static ref RAW_SELECTOR_PATTERN: Regex = {
@@ -34,17 +36,51 @@ fn _probably_path(value: &str) -> bool {
     }
 }
 
+// TODO: Use builder type to validate at compile time
+struct SelectionCriteriaBuilder {
+    raw: String,
+    method: Option<MethodName>,
+    value: Option<String>,
+    childrens_parents: Option<bool>,
+    parents: Option<bool>,
+    parents_depth: Option<u64>,
+    children: Option<bool>,
+    children_depth: Option<u64>,
+    // TODO: Default to Eager
+    indirect_selection: Option<IndirectSelection>,
+}
+
+impl SelectionCriteriaBuilder {
+    pub fn new(raw: &str) -> SelectionCriteriaBuilder {
+        SelectionCriteriaBuilder{
+            raw: raw.to_owned(),
+            method: None,
+            value: None,
+            childrens_parents: None,
+            parents: None,
+            parents_depth: None,
+            children: None,
+            children_depth: None,
+            indirect_selection: None,
+        }
+    }
+}
+
 struct SelectionCriteria {
     raw: String,
     method: MethodName,
     method_arguments: Vec<String>,
+    value: String,
+    childrens_parents: bool,
+    parents: bool,
+    parents_depth: u64,
+    children: bool,
+    children_depth: u64,
+    // TODO: Default to Eager
+    indirect_selection: IndirectSelection,
 }
 
 impl SelectionCriteria {
-    fn split_on_method_separator(raw: &str) -> std::str::Split<&str> {
-        raw.split(".")
-    }
-
     fn default_method(value: &str) -> MethodName {
         let is_probably_path = _probably_path(value);
         let lowercase_value = value.to_lowercase();
@@ -56,15 +92,74 @@ impl SelectionCriteria {
         }
     }
 
-    pub fn from_single_spec(&self, raw: &str, indirect_selection: &IndirectSelection) -> Result<SelectionCriteria, &str> {
-        let result = RAW_SELECTOR_PATTERN.captures_iter(raw).peekable();
-    
-        if result.peek().is_some() {
-            // TODO: IMPL!!
-            todo!()
+    fn from_strings(
+        raw: &str, 
+        method: Option<&str>,
+        method_arguments: Vec<String>,
+        value: &str,
+        childrens_parents: &str,
+        parents: &str,
+        parents_depth: &str,
+        children: &str,
+        children_depth: &str,
+        indirect_selection: &str
+    ) -> SelectionCriteria {
+        SelectionCriteria {
+            raw: raw.to_owned(),
+            method: method.to_owned(),
+            method_arguments: method_arguments.to_owned(),
+            value: value.to_owned(),
+            childrens_parents: (),
+            parents: bool::from(parents),
+            parents_depth: (),
+            children: (),
+            children_depth: (),
+            indirect_selection: ()
         }
-        Err("Invalid selector spec")
+    }
 
+    fn from_captures(raw: &str, captures: Captures) -> SelectionCriteria {
+        /// TODO: if Match, get value and pass it along
+        /// if None, then get default value
+        let childrens_parents = captures.name("childrens_parents");
+        let parents = captures.name("parents");
+        let parents_depth = captures.name("parents_depth");
+        let method = captures.name("method");
+        let value = captures.name("value");
+        let children = captures.name("children");
+        let children_depth = captures.name("children_depth");
+
+        let mut builder = SelectionCriteriaBuilder::new(raw);
+
+        SelectionCriteria {
+            raw: raw.to_owned(),
+            method: captures.name("method").un().as_str(),
+            value: ,
+            childrens_parents: captures.name("childrens_parents"),
+            parents: captures.name("parents"),
+            parents_depth: captures.name("parents_depth"),
+            children: todo!(),
+            children_depth: todo!(),
+            indirect_selection: todo!()
+        }
+    }
+
+    fn parse_method_args(method_args: &str) -> Vec<&str> {
+        let mut result = method_args.split(".");
+        let mut result: VecDeque<&str> = result.collect();
+        result.pop_front();
+        Vec::from(result)
+    }
+
+    pub fn from_single_spec(raw: &str, indirect_selection: &IndirectSelection) -> Result<SelectionCriteria, &str> {
+        let result = RAW_SELECTOR_PATTERN.captures(raw);
+    
+        match result {
+            Some(captures) => {
+                Ok(SelectionCriteria::from_captures(raw, captures))
+            },
+            None => Err("Invalid selector spec")
+        }
     }
 }
 
