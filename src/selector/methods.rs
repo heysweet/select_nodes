95e19fe6c07a03bd73726d04b9433c32677697df
Andrew Sweet <andrewmsweet@gmail.com>
2023-03-26 11:40:52 -0400

    FQN implementation

--- src/selector/methods.rs
+++ src/selector/methods.rs
@@ -56,30 +56,38 @@ impl MethodName {
             return false;
         }
         let wildcard = vec!['*', '?', '[', ']'];
-        for (i, part) in selector_parts.into_iter().enumerate() {
+        for (i, part) in selector_parts.clone().into_iter().enumerate() {
             for char in &wildcard {
                 if part.contains(*char) {
-                    // PYTHON
-                    // # If we have a wildcard, we need to make sure that the selector matches the
-                    // # rest of the fqn, this is 100% backwards compatible with the old behavior of
-                    // # encountering a wildcard but more expressive in naturally allowing you to
-                    // # match the rest of the fqn with more advanced patterns
-                    // return fnmatch(
-                    //     ".".join(flat_fqn[slurp_from_ix:]),
-                    //     ".".join(node_selector.split(".")[slurp_from_ix:]),
-                    // )
-                    todo!()
+                    // If we have a wildcard, we need to make sure that the selector matches the
+                    // rest of the fqn, this is 100% backwards compatible with the old behavior of
+                    // encountering a wildcard but more expressive in naturally allowing you to
+                    // match the rest of the fqn with more advanced patterns
+                    let flat_fqn = flat_fqn[i..].join(".");
+                    let fnmatch = fnmatch_regex::glob_to_regex(flat_fqn.as_str());
+                    let remainder = &(selector_parts)[i..];
+                    let remainder = remainder.join(".");
+                    match fnmatch {
+                        Err(_) => false,
+                        Ok(fnmatch) => fnmatch.is_match(remainder.as_str()),
+                    };
                 }
             }
             if (flat_fqn[i] != part) {
-                return false
+                return false;
             }
         }
-        return true
+        return true;
     }
 
     fn is_node_match(&self, qualified_name: &str, fqn: &Vec<String>) -> bool {
-        true
+        if Self::is_selected_node(fqn, qualified_name) {
+            true
+        } else {
+            let unscoped_fqn = &fqn[1..].to_vec();
+            // Match nodes across different packages
+            Self::is_selected_node(unscoped_fqn, qualified_name)
+        }
     }
 
     pub fn search(
