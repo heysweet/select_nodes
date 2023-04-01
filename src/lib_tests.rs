e2d988c184be888f99aa26797af4fe24da193f1d
Andrew Sweet <andrewmsweet@gmail.com>
2023-03-31 20:36:54 -0400

    Restructure tests to have one coherent parantage

--- src/lib_tests.rs
+++ src/lib_tests.rs
@@ -1,94 +1,114 @@
 #[cfg(test)]
 mod select_nodes_tests {
-    use std::collections::{HashMap, HashSet};
-
     use crate::graph::node::GraphNode;
     use interface::NodeType::*;
 
     use super::super::*;
 
-    fn add_parents(
-        mut parents_map: HashMap<String, HashSet<String>>,
-        child_id: &str,
-        parent_ids: Vec<&str>,
-    ) -> HashMap<String, HashSet<String>> {
-        let child_id: String = child_id.into();
-        let parent_ids: Vec<UniqueId> = parent_ids
-            .iter()
-            .map(|id| {
-                let str: String = id.to_string();
-                str
-            })
-            .collect();
-        match parents_map.get(&child_id) {
-            Some(map) => {
-                let mut map = map.clone();
-                map.extend(parent_ids);
-                parents_map.insert(child_id.to_string(), map);
-                parents_map
-            }
-            None => {
-                let mut map = HashSet::new();
-                map.extend(parent_ids);
-                parents_map.insert(child_id.to_string(), map);
-                parents_map
-            }
-        }
-    }
-
+    /// Any node with an id: "PREFIX_" will have the node "PREFIX" as
+    /// a direct parent.
+    /// All nodes will have a parent where the number of chars in the
+    /// ID is a parent id ("and" is 3 chars, so a parent is "3")
     fn get_test_edges() -> Vec<Edge> {
         vec![
             Edge {
-                unique_id: "id_a".to_string(),
+                unique_id: "a".to_string(),
                 parents: vec![],
             },
             Edge {
-                unique_id: "id_b".to_string(),
-                parents: vec!["id_a".to_string()],
+                unique_id: "b".to_string(),
+                parents: vec![],
             },
             Edge {
-                unique_id: "id_c".to_string(),
-                parents: vec!["id_a".to_string(), "id_b".to_string()],
+                unique_id: "c".to_string(),
+                parents: vec![],
+            },
+            Edge {
+                unique_id: "d".to_string(),
+                parents: vec![],
+            },
+            Edge {
+                unique_id: "e".to_string(),
+                parents: vec![],
+            },
+            Edge {
+                unique_id: "f".to_string(),
+                parents: vec![],
+            },
+            Edge {
+                unique_id: "an".to_string(),
+                parents: vec!["a".to_string()],
+            },
+            Edge {
+                unique_id: "and".to_string(),
+                parents: vec!["an".to_string()],
+            },
+            Edge {
+                unique_id: "andr".to_string(),
+                parents: vec!["and".to_string()],
+            },
+            Edge {
+                unique_id: "andre".to_string(),
+                parents: vec!["andr".to_string()],
+            },
+            Edge {
+                unique_id: "andrew".to_string(),
+                parents: vec!["andre".to_string()],
+            },
+            Edge {
+                unique_id: "ab".to_string(),
+                parents: vec!["a".to_string()],
+            },
+            Edge {
+                unique_id: "abb".to_string(),
+                parents: vec!["ab".to_string()],
+            },
+            Edge {
+                unique_id: "abs".to_string(),
+                parents: vec!["ab".to_string()],
+            },
+            Edge {
+                unique_id: "abby".to_string(),
+                parents: vec!["abb".to_string()],
+            },
+            Edge {
+                unique_id: "ba".to_string(),
+                parents: vec!["b".to_string()],
+            },
+            Edge {
+                unique_id: "bar".to_string(),
+                parents: vec!["ba".to_string()],
+            },
+            Edge {
+                unique_id: "bat".to_string(),
+                parents: vec!["ba".to_string()],
+            },
+            Edge {
+                unique_id: "ca".to_string(),
+                parents: vec!["c".to_string()],
+            },
+            Edge {
+                unique_id: "car".to_string(),
+                parents: vec!["ca".to_string(), "bar".to_string()],
             },
         ]
+    }
+
+    fn make_node(id: impl Into<String>) -> Node {
+        let id: String = id.into();
+        Node {
+            unique_id: id.clone(),
+            name: format!("name_{}", &id),
+            resource_type: "model".to_string(),
+            package_name: format!("pkg_{}", &id),
+            path: format!("path_{}", &id),
+            original_file_path: format!("opath_{}", &id),
+            fqn: [format!("id_{}", &id)].to_vec(),
+        }
     }
 
     fn get_test_nodes() -> Vec<Node> {
-        vec![
-            Node {
-                unique_id: "id_a".to_string(),
-                name: "name_a".to_string(),
-                resource_type: "model".to_string(),
-                package_name: "pkg_a".to_string(),
-                path: "path_a".to_string(),
-                original_file_path: "opath_a".to_string(),
-                fqn: ["id_a".to_string()].to_vec(),
-            },
-            Node {
-                unique_id: "id_b".to_string(),
-                name: "name_b".to_string(),
-                resource_type: "analysis".to_string(),
-                package_name: "pkg_b".to_string(),
-                path: "path_b".to_string(),
-                original_file_path: "opath_b".to_string(),
-                fqn: ["id_b".to_string()].to_vec(),
-            },
-            Node {
-                unique_id: "id_c".to_string(),
-                name: "name_c".to_string(),
-                resource_type: "test".to_string(),
-                package_name: "pkg_c".to_string(),
-                path: "path_c".to_string(),
-                original_file_path: "opath_c".to_string(),
-                fqn: ["id_c".to_string()].to_vec(),
-            },
-        ]
-    }
-
-    fn test_parents_map() -> HashMap<String, HashSet<String>> {
-        let parents_map = HashMap::new();
-        let parents_map = add_parents(parents_map, "A", vec!["B"]);
-        add_parents(parents_map, "B", vec!["C"])
+        get_test_edges().iter().map(|edge| make_node(&edge.unique_id)).collect()
     }
 
     #[test]
@@ -113,13 +133,10 @@ mod select_nodes_tests {
         let edges = get_test_edges();
         let node_selector = NodeSelector::from(nodes, edges);
         let node_selector = node_selector.unwrap();
-        let result = node_selector.select_and_filter(
-            None,
-            &"id_a".to_string(),
-            &ResourceTypeFilter::All,
-        );
+        let result =
+            node_selector.select_and_filter(None, &"andr".to_string(), &ResourceTypeFilter::All);
 
-        let expected = vec!["id_a".to_string()];
+        let expected = vec!["andr".to_string()];
 
         assert!(result.is_ok());
         assert!(result.unwrap().eq(&expected));
